41b2edea74451b4d4389bbd1986b7051e7baf611
Michael Horn <michael-horn@northwestern.edu>
2013-09-04 17:24:19 -0500

    Commented out push again

--- dart/frog.dart
+++ dart/frog.dart
@@ -95,6 +95,7 @@ class Frog extends Turtle implements Touchable {
  * Push other frogs out of the way
  */
   void push(num distance) {
+    /*
     for (Frog frog in pond.getFrogsHere(this)) {
       double angle = angleBetween(frog);
       if (angle.abs() < 90.0) {
@@ -110,6 +111,7 @@ class Frog extends Turtle implements Touchable {
         }
       }
     }
+    */
   }
   
   
