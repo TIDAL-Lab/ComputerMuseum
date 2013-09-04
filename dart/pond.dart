3bf54b125d85ee131e9cae68d356d1b433546760
Michael Horn <michael-horn@northwestern.edu>
2013-09-04 11:25:20 -0500

    Fixed master timeout

--- dart/pond.dart
+++ dart/pond.dart
@@ -46,6 +46,8 @@ class FrogPond extends TouchManager {
   /* List of frogs */  
   List<Turtle> frogs = new List<Frog>();
   
+  /* Master timeout to restart exhibit after 80 seconds of inactivity */
+  int _countdown = 0;
   
   ImageElement pond = new ImageElement();
   
@@ -107,24 +109,14 @@ class FrogPond extends TouchManager {
     new Timer.periodic(const Duration(milliseconds : 40), animate);
     
     // master timeout
-    //resetMasterTimeout();
-    //document.documentElement.onMouseDown.listen((e) => resetMasterTimeout());
-    //document.documentElement.onTouchStart.listen((e) => resetMasterTimeout());
-  }
-  
-  
-/**
- * Exhibit reloads after 1 minute of inactivity
- */
-  Timer _master = null;
-  void resetMasterTimeout() {
-    if (_master != null) _master.cancel();
-    _master = new Timer(const Duration(seconds : 60), () {
-      print("timeout");
-      window.location.reload();
+    new Timer.periodic(const Duration(seconds : 10), (timer) {
+      _countdown += 10;
+      if (_countdown >= 80) window.location.reload();
     });
+    document.documentElement.onMouseDown.listen((e) => _countdown = 0);
+    document.documentElement.onTouchStart.listen((e) => _countdown = 0);
   }
-
+  
   
 /**
  * Add a frog to the pond
