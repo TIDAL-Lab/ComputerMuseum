fc96d96fda297f980afcb40358aab4bd3e641bc8
Michael Horn <michael-horn@northwestern.edu>
2013-09-11 14:55:21 -0500

    Movable lilypads

---
+++ dart/lilypad.dart
@@ -0,0 +1,86 @@
+/*
+ * Computer History Museum Frog Pond
+ * Copyright (c) 2013 Michael S. Horn
+ * 
+ *           Michael S. Horn (michael-horn@northwestern.edu)
+ *           Northwestern University
+ *           2120 Campus Drive
+ *           Evanston, IL 60613
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2) as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+part of ComputerHistory;
+
+
+class LilyPad extends Turtle implements Touchable {
+  
+  /* pond that contains this lilypad */
+  FrogPond pond;
+
+  double _lastX, _lastY;
+  
+  bool refresh = false;
+
+  
+  LilyPad(this.pond) : super() {
+    img.src = "images/lilypad.png";
+  }
+  
+  
+  bool animate() {
+    return false;
+  }
+
+  
+  void _drawLocal(CanvasRenderingContext2D ctx) {  
+    num iw = width;
+    num ih = height;
+    ctx.drawImageScaled(img, -iw/2, -ih/2, iw, ih);
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, PI * 2, true);
+    ctx.fill();
+  }
+  
+  
+  void move(num dx, num dy) {
+    x += dx;
+    y += dy;
+  }
+  
+
+  bool containsTouch(Contact c) {
+    num dist = distance(c.touchX, c.touchY, x, y);
+    return dist < radius;
+  }
+  
+  bool touchDown(Contact c) {
+    _lastX = c.touchX;
+    _lastY = c.touchY;
+    return true;
+  }
+
+  
+  void touchUp(Contact c) { }
+
+  
+  void touchDrag(Contact c) {
+    move(c.touchX - _lastX, c.touchY - _lastY);
+    _lastX = c.touchX;
+    _lastY = c.touchY;
+    refresh = true;
+  }
+  
+  void touchSlide(Contact c) { }
+}
