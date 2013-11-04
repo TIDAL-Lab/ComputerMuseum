f5920dce7c579c7f524b4313ce2eb6b84de49cc7
Michael Horn <michael-horn@northwestern.edu>
2013-11-04 02:16:10 -0600

    Moved pulse() here from frog

--- dart/turtle.dart
+++ dart/turtle.dart
@@ -109,6 +109,18 @@ abstract class Turtle {
   }
   
   
+  void pulse() {
+    tween = new Tween();
+    tween.function = TWEEN_SINE2;
+    tween.duration = 10;
+    tween.repeat = 3;
+    tween.ondelta = ((value) => opacity += value );
+    tween.addControlPoint(1.0, 0.0);
+    tween.addControlPoint(0.0, 0.5);
+    tween.addControlPoint(1.0, 1.0);    
+  }
+  
+  
   bool animate();
 
   
